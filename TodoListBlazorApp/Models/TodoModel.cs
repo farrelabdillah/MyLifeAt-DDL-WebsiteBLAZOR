4cb9dde92cc2d8c801d12c3f9b69527b2dfbdf57
farrelabdillah <farrelabdillah@users.noreply.github.com>
2024-09-06 19:08:24 +0700

    web blazor

---
+++ TodoListBlazorApp/Models/TodoModel.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoListBlazorApp.Models
+{
+    public class TodoModel
+    {
+        public string TodoId { get; set; }
+        public string Day { get; set; }
+        public DateTime TodayDate { get; set; }
+        public string Note { get; set; }
+        public int DetailCount { get; set; }
+        public List<TodoDetailModel> TodoDetails { get; set; } = new List<TodoDetailModel>();
+    }
+
+    public class TodoDetailModel
+    {
+        public string TodoDetailId { get; set; }
+        public string TodoId { get; set; }
+        public string Activity { get; set; }
+        public string Category { get; set; }
+        public string DetailNote { get; set; }
+    }
+}
---
+++ TodoListBlazorApp/Services/TodoService.cs
@@ -0,0 +1,28 @@
+﻿using TodoListBlazorApp.Models;
+using System.Net.Http.Json;
+
+namespace TodoListBlazorApp.Services
+{
+    public class TodoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TodoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<TodoModel>> GetTodosAsync(int pageNumber, int pageSize)
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<TodoModel>>($"https://todo-api.bestcar.id/api/v1/todo?pageNumber={pageNumber}&pageSize={pageSize}");
+            return response ?? new List<TodoModel>();
+        }
+
+        public async Task<bool> AddTodoAsync(TodoModel newTodo)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://todo-api.bestcar.id/api/v1/todo", newTodo);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
+
